7c9b99a1d1a055cb05df26a749ee2c45e6ac92a9
sergey <serjjiniuss@gmail.com>
2025-10-14 17:07:56 +0400

    add screenshot to assets

--- src/hooks/http.hook.ts
+++ src/hooks/http.hook.ts
@@ -1,50 +1,52 @@
 import { useState, useCallback } from "react";
 
-type HTTPRequestMethods = "GET" | "POST" | "PATCH" | "DELETE";
+type HTTPRequestMethods = "GET" | "POST" | "PATCH" | "DELETE" | "PUT";
 export type loadingStatusOptions = "idle" | "loading" | "error";
 interface HTTPHeaders {
-	[key: string]: string;
+    [key: string]: string;
 }
 // type HTTPHeaders = Record<string, string>
 
 interface RequestConfig {
-	url: string;
-	method?: HTTPRequestMethods;
-	body?: string | null;
-	headers?: HTTPHeaders;
+    url: string;
+    method?: HTTPRequestMethods;
+    body?: string | null;
+    headers?: HTTPHeaders;
 }
 
 export const useHttp = () => {
-	const [loadingStatus, setLoadingStatus] =
-		useState<loadingStatusOptions>("idle");
+    const [loadingStatus, setLoadingStatus] =
+        useState<loadingStatusOptions>("idle");
 
-	const request = useCallback(
-		async ({
-			url,
-			method = "GET",
-			body = null,
-			headers = { "Content-Type": "application/json" },
-		}: RequestConfig) => {
-			setLoadingStatus("loading");
+    const request = useCallback(
+        async ({
+            url,
+            method = "GET",
+            body = null,
+            headers = { "Content-Type": "application/json" },
+        }: RequestConfig) => {
+            setLoadingStatus("loading");
 
-			try {
-				const response = await fetch(url, { method, body, headers });
+            try {
+                const response = await fetch(url, { method, body, headers });
 
-				if (!response.ok) {
-					throw new Error(`Could not fetch ${url}, status: ${response.status}`);
-				}
+                if (!response.ok) {
+                    throw new Error(
+                        `Could not fetch ${url}, status: ${response.status}`
+                    );
+                }
 
-				const data = await response.json();
+                const data = await response.json();
 
-				setLoadingStatus("idle");
-				return data;
-			} catch (e) {
-				setLoadingStatus("error");
-				throw e;
-			}
-		},
-		[]
-	);
+                setLoadingStatus("idle");
+                return data;
+            } catch (e) {
+                setLoadingStatus("error");
+                throw e;
+            }
+        },
+        []
+    );
 
-	return { loadingStatus, request };
+    return { loadingStatus, request };
 };
--- src/services/AppointmentService.ts
+++ src/services/AppointmentService.ts
@@ -1,6 +1,6 @@
 import {
-	IAppointment,
-	ActiveAppointment,
+    IAppointment,
+    ActiveAppointment,
 } from "../shared/interfaces/appointment.interface";
 import { useHttp } from "../hooks/http.hook";
 import hasRequiredFields from "../utils/hasRequiredFields";
@@ -12,69 +12,75 @@ dayjs.extend(customParseFormat);
 const requiredFields = ["id", "date", "name", "service", "phone", "canceled"];
 
 const useAppointmentService = () => {
-	const { loadingStatus, request } = useHttp();
+    const { loadingStatus, request } = useHttp();
 
-	const _apiBase = "http://localhost:3001/appointments";
+    const _apiBase =
+        "https://67cc02203395520e6af6e27d.mockapi.io/api/v1/appointments";
 
-	const getAllAppointments = async (): Promise<IAppointment[]> => {
-		const res = await request({ url: _apiBase });
-		if (
-			res.every((item: IAppointment) => hasRequiredFields(item, requiredFields))
-		) {
-			return res.sort(function (a: IAppointment, b: IAppointment) {
-				return new Date(a.date).getTime() - new Date(b.date).getTime();
-			});
-		} else {
-			throw new Error("Data doesnt have all the fields");
-		}
-	};
+    const getAllAppointments = async (): Promise<IAppointment[]> => {
+        const res = await request({ url: _apiBase });
+        if (
+            res.every((item: IAppointment) =>
+                hasRequiredFields(item, requiredFields)
+            )
+        ) {
+            return res.sort(
+                (a: IAppointment, b: IAppointment) =>
+                    new Date(a.date).getTime() - new Date(b.date).getTime()
+            );
+        } else {
+            throw new Error("Data doesn't have all the fields");
+        }
+    };
 
-	const getAllActiveAppointments = async () => {
-		const base = await getAllAppointments();
-		const transformed: ActiveAppointment[] = base
-			.filter((item) => {
-				return !item.canceled && dayjs(item.date).diff(undefined, "minute") > 0;
-			})
-			.map((item) => {
-				return {
-					id: item.id,
-					date: item.date,
-					name: item.name,
-					service: item.service,
-					phone: item.phone,
-				};
-			});
-		return transformed;
-	};
+    const getAllActiveAppointments = async () => {
+        const base = await getAllAppointments();
+        const transformed: ActiveAppointment[] = base
+            .filter((item) => {
+                return (
+                    !item.canceled &&
+                    dayjs(item.date).diff(undefined, "minute") > 0
+                );
+            })
+            .map((item) => {
+                return {
+                    id: item.id,
+                    date: item.date,
+                    name: item.name,
+                    service: item.service,
+                    phone: item.phone,
+                };
+            });
+        return transformed;
+    };
 
-	const cancelOneAppointment = async (id: number) => {
-		return await request({
-			url: `${_apiBase}/${id}`,
-			method: "PATCH",
-			body: JSON.stringify({ canceled: true }),
-		});
-	};
+    const cancelOneAppointment = async (id: number) => {
+        return await request({
+            url: `${_apiBase}/${id}`,
+            method: "PUT",
+            body: JSON.stringify({ canceled: true }),
+        });
+    };
 
-	const createNewAppointment = async (body: IAppointment) => {
-		const id = new Date().getTime();
-		body["id"] = id;
-		body["date"] = dayjs(body.date, "DD/MM/YYYY HH:mm").format(
-			"YYYY-MM-DDTHH:mm"
-		);
-		return await request({
-			url: _apiBase,
-			method: "POST",
-			body: JSON.stringify(body),
-		});
-	};
+    const createNewAppointment = async (body: IAppointment) => {
+        body["date"] = dayjs(body.date, "DD/MM/YYYY HH:mm").format(
+            "YYYY-MM-DDTHH:mm"
+        );
 
-	return {
-		loadingStatus,
-		getAllAppointments,
-		getAllActiveAppointments,
-		cancelOneAppointment,
-		createNewAppointment,
-	};
+        return await request({
+            url: _apiBase,
+            method: "POST",
+            body: JSON.stringify(body),
+        });
+    };
+
+    return {
+        loadingStatus,
+        getAllAppointments,
+        getAllActiveAppointments,
+        cancelOneAppointment,
+        createNewAppointment,
+    };
 };
 
 export default useAppointmentService;
